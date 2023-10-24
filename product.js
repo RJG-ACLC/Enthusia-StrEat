404c0bc22968595ce87490c7fcf1d42185d88fc2
RJG-ACLC <rodgejorrel4@gmail.com>
2023-10-24 16:15:10 +0800

    1

--- product.js
+++ product.js
@@ -1,72 +1,142 @@
 let iconCart = document.querySelector('.iconCart');
-  let cart = document.querySelector('.cart');
-  let container = document.querySelector('.container');
-  let close = document.querySelector('.close');
+let cart = document.querySelector('.cart');
+let container = document.querySelector('.container');
+let close = document.querySelector('.close');
 
-    iconCart  .addEventListener('click', ()=> {
-      if(cart.style.right == '-100%'){
+iconCart.addEventListener('click', function(){
+    if(cart.style.right == '-100%'){
         cart.style.right = '0';
-        container.style.transform = 'translateX(-400px)';
-      }
-
-      else{
+        container.style.transform = 'translateX(-725px)';
+    }else{
         cart.style.right = '-100%';
         container.style.transform = 'translateX(0)';
-      }
-    })
+    }
+})
+close.addEventListener('click', function (){
+    cart.style.right = '-100%';
+    container.style.transform = 'translateX(0)';
+})
 
-    .close.addEventListener('click', ()=> {
-      cart.style.right = '-100%';
-      container.style.transform = 'translateX(0)';
-    })
 
-    let products = null;
-    fetch('product.json')
+let products = null;
+// get data from file json
+fetch('product.json')
     .then(response => response.json())
     .then(data => {
-      products = data;
-      addDataToHTML();
-    })
+        products = data;
+        addDataToHTML();
+})
 
-    function addDataToHTML(){
-      let listProductHTML = document.querySelector('.listProduct')
-      listProductHTML.innerHTML = '';
+//show datas product in list 
+function addDataToHTML(){
+    // remove datas default from HTML
+    let listProductHTML = document.querySelector('.listProduct');
+    listProductHTML.innerHTML = '';
 
-      if(products != null){
-          products.array.forEach(product => {
+    // add new datas
+    if(products != null) // if has data
+    {
+        products.forEach(product => {
             let newProduct = document.createElement('div');
             newProduct.classList.add('food-items');
-            newProduct.innerHTML =
-            '<img src="${product.image}"> <div class="details"> <div class="details-sub"> <h5>${product.name}</h5> <h5 class="price">$${product.price}</h5> </div> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit reiciendis nam non quia! Earum eveniet minus. Facilis explicabo natus nihil voluptatem eveniet pariatur.</p> <button onclick="addCart(${product.id})">Add To Cart</button> </div>'
+            newProduct.innerHTML = 
+            `<img src="${product.image}" alt="${product.name}">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>${product.name}</h5>
+                    <h5 class="price"> ₱${product.price} </h5>
+                </div>
+                <p>${product.description}</p>
+                <button onclick="addCart(${product.id})">Add To Cart</button>
+            </div>`;
+
             listProductHTML.appendChild(newProduct);
-          });
-      }
-    }
-    let listCart = [];
-    function checkCart (){
-        var cookieValue = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('listCart='));
-        if(cookieValue){
-            listCart = JSON.parse(cookieValue.split('=')[1]);
-        }
-    }
-    checkCart();
-    function addCart($idProduct){
-        let productCopy = JSON.parse(JSON.stringify(products));
-        if(!listCart[$idProduct]){
-            let dataProduct = productCopy.filter(
-                product => product.id == $idProduct
-            )[0];
 
-            listCart[$idProduct] = dataProduct;
-            listCart[$idProduct].quantity = 1;
-        } else{
-            listCart[$idProduct].quantity++;
-
-        }
-        let timeSave = "expires=Thu, 31 Dec 2025 23:59:59 UTC+8";
-        document.cookie = "listCart="+JSON.stringify(listCart)+"; "+timeSave+"; path=/;";
-        addCartToHTML();
+        });
     }
+}
+//use cookie so the cart doesn't get lost on refresh page
+
+
+let listCart = [];
+function checkCart(){
+    var cookieValue = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('listCart='));
+    if(cookieValue){
+        listCart = JSON.parse(cookieValue.split('=')[1]);
+    }else{
+        listCart = [];
+    }
+}
+checkCart();
+function addCart($idProduct){
+    let productsCopy = JSON.parse(JSON.stringify(products));
+    //// If this product is not in the cart
+    if(!listCart[$idProduct]) 
+    {
+        listCart[$idProduct] = productsCopy.filter(product => product.id == $idProduct)[0];
+        listCart[$idProduct].quantity = 1;
+    }else{
+        //If this product is already in the cart.
+        //I just increased the quantity
+        listCart[$idProduct].quantity++;
+    }
+    document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
+
     addCartToHTML();
+}
+addCartToHTML();
+function addCartToHTML(){
+    // clear data default
+    let listCartHTML = document.querySelector('.listCart');
+    listCartHTML.innerHTML = '';
+
+    let totalHTML = document.querySelector('.totalQuantity');
+    let totalQuantity = 0;
+    // if has product in Cart
+    if(listCart){
+        listCart.forEach(product => {
+            if(product){
+                let newCart = document.createElement('div');
+                newCart.classList.add('item');
+                newCart.innerHTML = 
+                    `<img src="${product.image}">
+                    <div class="content">
+                        <div class="name">${product.name}</div>
+                        <div class="price">₱${product.price} / 1 product</div>
+                    </div>
+                    <div class="quantity">
+                        <button onclick="changeQuantity(${product.id}, '-')">-</button>
+                        <span class="value">${product.quantity}</span>
+                        <button onclick="changeQuantity(${product.id}, '+')">+</button>
+                    </div>`;
+                listCartHTML.appendChild(newCart);
+                totalQuantity = totalQuantity + product.quantity;
+            }
+        })
+    }
+    totalHTML.innerText = totalQuantity;
+}
+function changeQuantity($idProduct, $type){
+    switch ($type) {
+        case '+':
+            listCart[$idProduct].quantity++;
+            break;
+        case '-':
+            listCart[$idProduct].quantity--;
+
+            // if quantity <= 0 then remove product in cart
+            if(listCart[$idProduct].quantity <= 0){
+                delete listCart[$idProduct];
+            }
+            break;
+    
+        default:
+            break;
+    }
+    // save new data in cookie
+    document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
+    // reload html view cart
+    addCartToHTML();
+}
