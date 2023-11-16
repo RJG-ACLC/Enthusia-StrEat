21cbd057750edb86a431bdf8be87bdbd65f6059c
RJG-ACLC <rodgejorrel4@gmail.com>
2023-11-16 14:35:16 +0800

    1

--- EDT.js
+++
@@ -1,2 +0,0 @@
-let x = Math.floor((Math.random() * 15) + 15);
-document.getElementById("demo").innerHTML = x;
--- SYT.js
+++
@@ -1,2 +0,0 @@
-let x = Math.floor((Math.random() * 50) + 50);
-document.getElementById("demo").innerHTML = x;
